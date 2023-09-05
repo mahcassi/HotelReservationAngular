bf1849f09eeb834c8f993fc88af3e27b9f713a32
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-04 21:21:55 -0300

    created hotel modules

---
+++ src/app/pages/hotel/hotel.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchHotelComponent } from './search-hotel/search-hotel.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchHotelComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HotelModule { }
