73416dac14967283dac72e29cd6e4e732bafeb87
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-04 21:22:46 -0300

    created first hotel component

---
+++ src/app/pages/hotel/search-hotel/search-hotel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-hotel',
+  templateUrl: './search-hotel.component.html',
+  styleUrls: ['./search-hotel.component.css']
+})
+export class SearchHotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
