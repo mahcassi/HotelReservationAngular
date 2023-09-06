157bc8eb6fc810046edfa9f028ce1323d593101d
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-05 22:54:14 -0300

    adjust app-routing with hotel module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: 'hotel',
+    pathMatch: 'full',
+  },
+  {
+    path: 'hotel',
+    loadChildren: () => import("../app/pages/hotel/hotel.module").then((m) => m.HotelModule)
+  },
 ];
 
 @NgModule({
