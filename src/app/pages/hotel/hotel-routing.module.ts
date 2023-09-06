8a47484170600e3800065cb807bbfef8f727f9c3
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-05 22:54:35 -0300

    add first hotel route

--- src/app/pages/hotel/hotel-routing.module.ts
+++ src/app/pages/hotel/hotel-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchHotelComponent } from './search-hotel/search-hotel.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchHotelComponent,
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class HotelRoutingModule { }
+export class HotelRoutingModule {}
--- src/app/pages/hotel/hotel.module.ts
+++ src/app/pages/hotel/hotel.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchHotelComponent } from './search-hotel/search-hotel.component';
+import { HotelRoutingModule } from './hotel-routing.module';
 
 
 
 @NgModule({
-  declarations: [
-    SearchHotelComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SearchHotelComponent],
+  imports: [CommonModule, HotelRoutingModule],
 })
-export class HotelModule { }
+export class HotelModule {}
