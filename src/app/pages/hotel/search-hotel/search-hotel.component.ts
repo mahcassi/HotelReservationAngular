44ac1b6fa9c3737530da76a5ecc4c859c4fd8052
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-07 20:42:49 -0300

    Creted hotel list and displaying in frontend

--- src/app/pages/hotel/search-hotel/search-hotel.component.ts
+++ src/app/pages/hotel/search-hotel/search-hotel.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { IHotelResponse } from 'src/app/interfaces/hotel.interface';
+import { HotelService } from 'src/app/services/hotel.service';
 
 @Component({
   selector: 'app-search-hotel',
   templateUrl: './search-hotel.component.html',
-  styleUrls: ['./search-hotel.component.css']
+  styleUrls: ['./search-hotel.component.css'],
 })
 export class SearchHotelComponent implements OnInit {
+  hotels: IHotelResponse[] = [];
 
-  constructor() { }
+  constructor(private _hotelService: HotelService) {}
 
   ngOnInit(): void {
+    this.searchHotels();
   }
 
+  searchHotels() {
+    this._hotelService.getHotels().subscribe((res) => {
+      this.hotels = res;
+    });
+  }
 }
