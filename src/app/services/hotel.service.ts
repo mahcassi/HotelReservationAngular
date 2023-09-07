2631cabb6a4614eb5b0e59a1120e49637da85fb2
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-07 20:39:38 -0300

    created hotel service to get hotels

---
+++ src/app/services/hotel.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IHotelResponse } from '../interfaces/hotel.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HotelService {
+  constructor(private _http: HttpClient) {}
+
+  getHotels(): Observable<IHotelResponse[]> {
+    return this._http.get<IHotelResponse[]>(`${environment.apiUrl}Hotels`);
+  }
+
+  getHotelById(id: number): Observable<any> {
+    return this._http.get<any>(`${environment.apiUrl}Hotels/${id}`);
+  }
+}
