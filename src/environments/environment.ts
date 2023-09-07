d0bc33a086011da381974fb9efac634f25f81d7c
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-07 20:40:05 -0300

    add apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const apiUrl = "https://localhost:7123/api/";
+
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: apiUrl,
 };
 
 /*
