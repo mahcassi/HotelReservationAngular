61f05ac04d4ea2a4f2219968b0cf2f1af32583b5
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-11 21:50:27 -0300

    Created room component

---
+++ src/app/pages/hotel/details-hotel/components/rooms/rooms.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IRoom } from 'src/app/interfaces/room.interface';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css'],
+})
+export class RoomsComponent implements OnInit {
+  @Input() rooms: IRoom[] = [];
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/pages/hotel/hotel.module.ts
+++ src/app/pages/hotel/hotel.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SearchHotelComponent } from './search-hotel/search-hotel.component';
 import { HotelRoutingModule } from './hotel-routing.module';
 import { DetailsHotelComponent } from './details-hotel/details-hotel.component';
+import { RoomsComponent } from './details-hotel/components/rooms/rooms.component';
 
 
 
 @NgModule({
-  declarations: [SearchHotelComponent, DetailsHotelComponent],
+  declarations: [SearchHotelComponent, DetailsHotelComponent, RoomsComponent],
   imports: [CommonModule, HotelRoutingModule],
 })
 export class HotelModule {}
