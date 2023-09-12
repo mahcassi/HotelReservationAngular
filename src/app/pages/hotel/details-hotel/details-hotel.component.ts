d308e39a9e0dfc1606d7237380b8e7ae7b842764
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-09-11 21:33:01 -0300

    integration hotel details

--- src/app/pages/hotel/details-hotel/details-hotel.component.ts
+++ src/app/pages/hotel/details-hotel/details-hotel.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IHotelResponse } from 'src/app/interfaces/hotel.interface';
+import { HotelService } from 'src/app/services/hotel.service';
 
 @Component({
   selector: 'app-details-hotel',
   templateUrl: './details-hotel.component.html',
-  styleUrls: ['./details-hotel.component.css']
+  styleUrls: ['./details-hotel.component.css'],
 })
 export class DetailsHotelComponent implements OnInit {
+  detailsHotelResponse!: IHotelResponse;
 
-  constructor() { }
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _hotelService: HotelService
+  ) {}
 
   ngOnInit(): void {
-  }
+    const hotelId = this._activatedRoute.snapshot.params['id'];
 
+    this._hotelService.getHotelById(hotelId).subscribe((res) => {
+      this.detailsHotelResponse = res;
+    });
+  }
 }
